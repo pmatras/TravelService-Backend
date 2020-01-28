c7ea484bd779758dd5b0414ea6628a394dcadd65
Piotr Matras <piotrmatras21@gmail.com>
2020-01-28 23:13:26 +0100

    configuration changed

--- TravelService/src/main/java/io/travelservice/TravelService/TravelServiceApplication.java
+++ TravelService/src/main/java/io/travelservice/TravelService/TravelServiceApplication.java
@@ -2,8 +2,10 @@ package io.travelservice.TravelService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "io.travelservice.rest","io.travelservice.database"} )
 public class TravelServiceApplication {
 
 	public static void main(String[] args) {
--- TravelService/src/main/java/io/travelservice/rest/LoginApi.java
+++ TravelService/src/main/java/io/travelservice/rest/LoginApi.java
@@ -1,17 +1,16 @@
 package io.travelservice.rest;
 
+import io.travelservice.database.UsersRepository;
 import io.travelservice.user.User;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Configuration
 @RestController
 public class LoginApi {
 
     @PostMapping("/login")
-    public boolean loginUser(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password) {
+    public boolean login(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password) {
         return User.authenticateUser(username, password);
     }
 }
